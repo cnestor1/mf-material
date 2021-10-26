1d39d6a87ad90dddcf190d84fe90343f1eb7dd68
Cyril Nestor <cnestor@ibm.com>
2021-10-25 20:24:33 -0400

    Use Azure remotes

--- src/app/tab1/tab1.component.ts
+++ src/app/tab1/tab1.component.ts
@@ -10,29 +10,11 @@ export class Tab1Component implements OnInit {
 
     constructor() { }
     items: WebComponentWrapperOptions[] = [
-      // {
-      //   remoteEntry: 'http://localhost:4220/remoteEntry.js',
-      //   remoteName: 'logmsgml',
-      //   exposedModule: './web-components',
-      //   elementName: 'logmsgml-element'
-      // },
       {
-        remoteEntry: 'http://localhost:4204/remoteEntry.js',
-        remoteName: 'mfe4',
+        remoteEntry: 'https://witty-wave-0a695f710.azurestaticapps.net/remoteEntry.js',
+        remoteName: 'react',
         exposedModule: './web-components',
-        elementName: 'mfe4-element'
-      },
-      // {
-      //   remoteEntry: 'http://localhost:4203/remoteEntry.js',
-      //   remoteName: 'mfe3',
-      //   exposedModule: './web-components',
-      //   elementName: 'mfe3-element'
-      // },
-      {
-        remoteEntry: 'http://localhost:4212/remoteEntry.js',
-        remoteName: 'angular1',
-        exposedModule: './web-components',
-        elementName: 'angular1-element'
+        elementName: 'react-element'
       },
       {
         remoteEntry: 'https://gray-river-0b8c23a10.azurestaticapps.net/remoteEntry.js',
@@ -40,7 +22,7 @@ export class Tab1Component implements OnInit {
         exposedModule: './web-components',
         elementName: 'angular3-element'
       }
-      
+
     ];
 
   ngOnInit(): void {
