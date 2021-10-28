771a5b375496086c4339409f7da8115f2da9b6f7
Cyril Nestor <cnestor@ibm.com>
2021-10-28 15:10:51 -0400

    Use div for content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,13 @@
 import { Component, NgZone } from '@angular/core';
 import { shareNgZone } from '@angular-architects/module-federation-tools';
+import { Tab1Component } from './tab1/tab1.component';
+
+interface TabObj {
+  label: string,
+  content: any,
+  active: boolean,
+  component: any
+}
 
 
 @Component({
@@ -9,8 +17,23 @@ import { shareNgZone } from '@angular-architects/module-federation-tools';
 })
 export class AppComponent {
   title = 'angular12-example';
-  
+
+  tabs: TabObj[] = [{ label: 'tab1', content: '<app-tab1></app-tab1>', active: true, component: Tab1Component },
+  { label: 'tab2', content: 'tab2', active: false, component: null },
+  { label: 'tab3', content: 'tab3', active: false, component: null }];
+
+
   constructor(private ngZone: NgZone) {
     shareNgZone(ngZone);
+  }
+
+  tabChanged($event) {
+    console.log($event);
+
+    for (const ttab of this.tabs) {
+      ttab.active = false;
+    }
+    this.tabs[$event.index].active = true;
+    console.log("#### tabs changed: " + this.tabs[$event.index].label + " - " + this.tabs[$event.index].active)
   }
 }
